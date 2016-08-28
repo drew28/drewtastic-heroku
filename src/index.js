f578e86435457e9a8dcc6b9c712bdb1e88a470d0
drew28 <atreid@gmail.com>
2016-08-28 14:08:27 -0400

    Turn off analytics for localhost.

--- src/index.js
+++ src/index.js
@@ -14,9 +14,17 @@ import ReactGA from 'react-ga';
 
 ReactGA.initialize('UA-5940128-2');
 
+const isLocalhost = !!(window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
+
 function logPageView() {
-  ReactGA.set({page: window.location.pathname});
-  ReactGA.pageview(window.location.pathname);
+  if (!isLocalhost) {
+    ReactGA.set({page: window.location.pathname});
+    ReactGA.pageview(window.location.pathname);
+ }
 }
 
 ReactDOM.render((
